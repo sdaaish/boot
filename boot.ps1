c864cb591c69b0f58a994a6efaf12e3fcce7e0db
Stig Dahl <stig@charlottendal.net>
2019-09-30 12:20:34 +0200

    Initial config

---
+++ boot.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+Simple installation script for new computers
+
+.DESCRIPTION
+To be done
+
+.PARAMETER Foobar
+none
+
+.EXAMPLE
+tbd
+
+.NOTES
+Some notes
+#>
+mkdir ~/tmp
+mkdir ~/repos
+mkdir ~/bin
+git clone https://github.com/sdaaish/powershell-stuff ~/repos/powershell-stuff
---
+++ boot.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Setup stuff for initial setup
+
+mkdir ~/tmp ~/bin ~/repos ~/.ssh ~/.gnupg
+
+sudo apt install --yes git make tmux
+git clone https://github.com/sdaaish/dotfiles.git ~/repos/dotfiles
+make -C ~/repos/dotfiles
+.
+# Source the new profile
+~/.profile
+
+# Install packages and settings
+install-emacs-d
+install-emacs-snapshot
+install-git-latest
+#install-lxss-basic
+install-domain-tool
+#install-net-stuff
+#install-fun-stuff
+install-powershell
+#install-jekyll
+#install-keybase-full
+install-keybase-cli
+#install-mailtools
+#install-keepass
+#install-docker-for-wsl
+#install-syncthing
+
+#get-bbk
+get-base16
+get-powerline-fonts
+get-tmux-plugin-manager
