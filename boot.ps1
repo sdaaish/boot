9f13684ec34aa398e154be5dfe025e81e8063ae9
Stig Dahl <stig@charlottendal.net>
2020-03-06 14:34:36 +0100

    Updating

--- boot.ps1
+++ boot.ps1
@@ -14,7 +14,21 @@ tbd
 .NOTES
 Some notes
 #>
-mkdir ~/tmp
-mkdir ~/repos
-mkdir ~/bin
-git clone https://github.com/sdaaish/powershell-stuff ~/repos/powershell-stuff
+$homedir = $env:USERPROFILE
+$dirs = @(
+    "tmp"
+    "repos"
+    "bin"
+)
+foreach($dir in $dirs){
+    New-Item -Path (Join-Path -Path $homedir -ChildPath $dir) -ItemType Directory -Force
+}
+$gitrepos = @{
+    "powershell-stuff" = "https://github.com/sdaaish/powershell-stuff.git", "develop"
+}
+foreach($repo in $gitrepos.GetEnumerator()){
+    $src = $repo.value[0]
+    $branch = $repo.value[1]
+    $destpath = Join-Path -Path $homedir -ChildPath "repos" -AdditionalChildPath $repo.key
+    git -C $destpath clone -b $branch $src
+}
