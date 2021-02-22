d6cb889bc6dfdd520e7bb74c8b9c6fa7ef3d294e
Stig Dahl <stig@charlottendal.net>
2021-02-22 20:02:51 +0100

    Adjust sytax of variables

--- boot.sh
+++ boot.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-export ID=$(awk -F "=" '/DISTRIB_ID/{print $2}' /etc/lsb-release)
-export RELEASE=$(awk -F "=" '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-export CODENAME==$(awk -F "=" '/DISTRIB_CODENAME/{print $2}' /etc/lsb-release)
+ID=$(awk -F "=" '/DISTRIB_ID/{print $2}' /etc/lsb-release)
+export ID
+
+RELEASE=$(awk -F "=" '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+export RELEASE
+
+CODENAME=$(awk -F "=" '/DISTRIB_CODENAME/{print $2}' /etc/lsb-release)
+export CODENAME
 
 # Setup stuff for initial setup
 
@@ -12,7 +17,7 @@ sudo apt install --yes git make tmux
 git clone --depth 1 https://github.com/sdaaish/boot.git ~/repos/boot
 git clone --depth 1 https://github.com/sdaaish/dotfiles.git ~/.config/dotfiles
 
-cd ~/.config/dotfiles
+cd ~/.config/dotfiles || exit
 ./setup.sh
 
 # Source the new profile
