2c3e13c7f479a2e54de31a4538e0e50d71f369e0
Stig Dahl <stig@charlottendal.net>
2021-08-31 17:24:37 +0200

    Fixed some issues

--- boot.ps1
+++ boot.ps1
@@ -52,18 +52,20 @@ $RepoSource = @{
 }
 
 # Set defaults
+Write-Verbose "Installing NuGet"
 if (-not $isLinux) {
     try {
-        Get-Package -Name NuGet
+        Get-PackageProvider -Name NuGet -ErrorAction Stop| Out-Null
     }
     catch {
         Install-PackageProvider -Name NuGet -Scope CurrentUser -Force -ForceBootStrap
     }
 }
 
+Write-Verbose "Installing modules"
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name PowerShellGet -Repository PSGallery -Force
-Install-Module BuildHelpers -Repository PSGallery -Force
+Install-Module -Name PowerShellGet -Repository PSGallery -Scope CurrentUser -MinimumVersion 2.2.5 -AllowClobber -Force
+Install-Module -Name BuildHelpers -Repository PSGallery -Scope CurrentUser -Force
 
 ## Create directories in $env:USERPROFILE
 foreach($dir in $dirs){
@@ -81,21 +83,24 @@ $LocalRepositorySplat = @{
 }
 
 # Register the repository
+Write-Verbose "Registering my module."
 Register-PSRepository @LocalRepositorySplat
-Install-Module -Name MyModule -Repository $RepoSource.Name -Force
+Install-Module -Name MyModule -Repository $RepoSource.Name -Scope CurrentUser -Force
 Import-Module MyModule -Force
 
 if (-not $isLinux){
+    Write-Verbose "Installing scoop."
     Install-Scoop
     try {
         & scoop install git
     }
     catch {
-        Write-Error "Scoop already installed."
+        Write-Error "git already installed."
     }
 }
 
 # Clone my github repositories
+Write-Verbose "Downloading repositories from github. "
 foreach($repo in $gitrepos.GetEnumerator()){
     $src = $repo.value[0]
     $branch = $repo.value[1]
@@ -104,7 +109,7 @@ foreach($repo in $gitrepos.GetEnumerator()){
     $destpath = Join-Path -Path $path -ChildPath $repo.key
 
     try {
-        Write-Host "Cloning ${src} to ${destpath}" 
+        Write-Host "Cloning ${src} to ${destpath}"
         git -C $path clone -b $branch $src $destpath
     }
     catch {
@@ -114,5 +119,7 @@ foreach($repo in $gitrepos.GetEnumerator()){
 
 # Set the executionpolicy for the system and not just the process. Only for Windows
 if (-not $isLinux) {
-    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force -Verbose
+    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force -Verbose -ErrorAction Ignore
 }
+
+Stop-Transcript
