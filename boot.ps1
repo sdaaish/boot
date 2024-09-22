9a802b87fc3769944e377ae423901c50e1cb1e41
Stig Dahl <stig@charlottendal.net>
2024-09-22 12:14:41 +0200

    Switch to new installation method with chezmoi for the boot.ps1

--- boot.ps1
+++ boot.ps1
@@ -1,193 +1,57 @@
 <#
 .SYNOPSIS
-Simple installation script for new computers
+Boot script for new Windows installation.
 
 .DESCRIPTION
-To be done
-
-.PARAMETER Foobar
-none
-
-.EXAMPLE
-tbd
-
-.NOTES
-Some notes
+Installs WinGet and dependencies if not already installed, and then Chezmoi to be able to boostrap a new device.
 #>
-[cmdletbinding()]
-Param ()
 
-# Enable logging
-Start-Transcript
-
-# To check if admin
-Function Test-Administrator {
+function Install-Winget {
+    [cmdletbinding()]
     param()
 
-    # Check for admin rights
-    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
-    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-    $prp.IsInRole($adm)
-}
+    process {
+        $progressPreference = 'SilentlyContinue'
+        Write-Information "Downloading WinGet and its dependencies..."
 
-if (-not (Test-Administrator)) {
-    throw "Need to run this Administrator"
-}
+        $downloads = Join-Path (Resolve-Path ${env:USERPROFILE}) Downloads
+        pushd $downloads
 
-# Add certificates if they exist. Useful for testing the build.
-$Path = Join-Path $PSScriptRoot Build
-Get-ChildItem -Path $Path -File -Filter *.cer | Foreach-Object {
-    Write-verbose "Installing certificate: ${$_.FullName}"
-    certutil -addstore Root $_.FullName
-}
+        Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
 
-# Variables
-if (-not $isLinux){     # Windows
-    $homedir = $env:USERPROFILE
-}
-else {
-    $homedir = $ENV:HOME
-}
+        Write-Information "Adding packages"
+        Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+        Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-# Directories to create
-$dirs = @(
-    "tmp"
-    "repos"
-    "bin"
-    "work"
-    "code"
-    ".local"
-    ".local/WindowsPowerShell/Modules"
-    ".local/PowerShell/Modules"
-    ".config/git"
-)
-
-## Create directories in $env:USERPROFILE
-foreach($dir in $dirs){
-    Write-Host "Creating directory $dir"
-    New-Item -Path (Join-Path -Path $homedir -ChildPath $dir) -ItemType Directory -Force|Out-Null
-}
-
-# Set defaults
-Write-Verbose "Installing NuGet"
-if (-not $isLinux) {
-    try {
-        Get-PackageProvider -Name NuGet -ForceBootStrap -ErrorAction Stop| Out-Null
-    }
-    catch {
-        Install-PackageProvider -Name NuGet -Scope CurrentUser -Force -ForceBootStrap
-    }
-    finally {
-        Register-PackageSource -Name nuget.org -Location https://www.nuget.org/api/v2 -ProviderName NuGet
-        Set-PackageSource -Name nuget.org -Trusted
+        Write-Information "Cleaning up"
+        Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx -Force
+        Remove-Item Microsoft.UI.Xaml.2.8.x64.appx -Force
+        Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -Force
     }
 }
 
-# Save modules to local storage
-if ($isLinux){
-    $ModulePath = Resolve-Path "~/.local/share/powershell/Modules"
-} else {
-    # Check wich version of Powershell
-    switch ($PSVersionTable.PSEdition){
-        "Core" {$version = "PowerShell/Modules"}
-        "Desktop" { $version = "WindowsPowerShell/Modules"}
-    }
-    $ModulePath = Join-Path -Path (Resolve-Path "~/.local") -ChildPath $version
+Function refreshenv {
+    $paths = @(
+				([System.Environment]::GetEnvironmentVariable("Path", "Machine") -split ([io.path]::PathSeparator))
+				([System.Environment]::GetEnvironmentVariable("Path", "User") -split ([io.path]::PathSeparator))
+    )
+    $env:path = ($paths | Select-Object -Unique) -join ([io.path]::PathSeparator)
 }
 
-# Add local Module-directory to ModulePath
-$env:PSModulePath = $env:PSModulePath + ";${ModulePath};"
+# Install WinGet if not already installed
+try { Get-Command winget.exe -ErrorAction Stop }
+catch { Install-Winget }
 
-Write-Verbose "Installing modules"
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+# Update the path
+refreshenv
 
-Save-Module -Name PowerShellGet -Path $ModulePath -Repository PSGallery -MinimumVersion 2.2.5 -Force
-Save-Module -Name BuildHelpers -Path $ModulePath -Repository PSGallery -Force
-Save-Module -Name Posh-Git -Path $ModulePath -Repository PSGallery -Force
-Save-Module -Name Terminal-Icons -Path $ModulePath -Repository PSGallery -Force
+# Install chezmoi
+try { Get-Command chezmoi.exe -ErrorAction Stop}
+catch { winget install --id twpayne.chezmoi --source winget --accept-source-agreements }
 
-switch ($version) {
-    "Desktop" {Save-Module PSReadLine -Path 'C:\Program Files\WindowsPowerShell\Modules\' -Force}
-    "Core" {Save-Module PSReadLine -Path 'C:\Program Files\PowerShell\Modules\' -Force}
-}
+refreshenv
 
-# The settings for my local Powershell Modules Repository
-$LocalRepositorySplat = @{
-    Name = "AzurePowershellModules"
-    SourceLocation = "https://pkgs.dev.azure.com/sdaaish/PSModules/_packaging/AzurePSModuleRepo/nuget/v2"
-    ScriptSourceLocation = "https://pkgs.dev.azure.com/sdaaish/PSModules/_packaging/AzurePSModuleRepo/nuget/v2"
-    InstallationPolicy = "Trusted"
-    PackageManagementProvider = $RepoSource.Provider
-    Provider = "NuGet"
-}
-
-# Register the repository for PowerShellGet, the old method
-Write-Verbose "Registering my module."
-Register-PSRepository @LocalRepositorySplat -ErrorAction Ignore
-Save-Module -Name MyModule -Path $ModulePath -Repository $RepoSource.Name -Force
-Import-Module MyModule -Force
-
-# Register the repository for Microsoft.PowerShell.PSResourceGet
-# This supports version 3
-$RepoSource = @{
-    Name = "AzurePowershellModules"
-    Uri = "https://pkgs.dev.azure.com/sdaaish/PSModules/_packaging/AzurePSModuleRepo/nuget/v3/index.json"
-    Trusted = $true
-    Priority = 40
-}
-
-Register-PSResourceRepository @RepoSource
-
-if (-not $isLinux){
-    Write-Verbose "Installing winget."
-    try {
-        Install-Winget -ErrorAction Stop
-    }
-    catch {
-        throw "Failed to install winget"
-    }
-}
-
-# Install software with Winget
-$options = @("--source", "winget", "--accept-package-agreements", "--accept-source-agreements")
-$silent = @($options, "--silent")
-& winget install Git.Git $options
-& winget install 7zip.7zip $silent
-& winget install Microsoft.PowerShell $silent
-& winget install Starship.Starship $silent
-& winget install twpayne.chezmoi $silent
-
-# Windows Terminal
-& winget install  Microsoft.WindowsTerminal $options  --scope user
-
-# Create path to make the rest work
-$path =  $(
-    'C:\Program Files\PowerShell\7'
-    'C:\Program Files\Git\bin'
-    'C:\Program Files\7-Zip'
-    'C:\Program Files\starship\bin\'
-)
-
-$oldpath =$env:path -split ";"
-$env:path = (($oldpath + $path) -join ";") -replace ";{2,}",";"
-
-$fileName = ".config{0}powershell{0}profile.ps1" -f [System.IO.Path]::DirectorySeparatorChar
-$text = @"
-`$StartTime = [System.Diagnostics.Stopwatch]::new()
-. $(Join-Path (Resolve-Path ~) $fileName))
-"@
-
-# Set the executionpolicy for the system and not just the process. Only for Windows
-if (-not $isLinux) {
-    $DesktopProfile = powershell -Command {$profile} -NoLogo -NoProfile -ExecutionProfile Bypass
-    $CoreProfile = pwsh -Command {$profile} -NoLogo -NoProfile -ExecutionProfile Bypass
-    New-Item -Path (Split-Path $DesktopProfile -Parent) -ItemType Directory -Force|Out-Null
-    New-Item -Path (Split-Path $CoreProfile -Parent) -ItemType Directory -Force|Out-Null
-    Add-Content -Path $DesktopProfile -Value $text
-    Add-Content -Path $CoreProfile -Value $text
-
-    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force -Verbose -ErrorAction Ignore
-}
-
-Stop-Transcript
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
